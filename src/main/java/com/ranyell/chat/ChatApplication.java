74465816fcc959a934e097a062c121cf188d78fd
RanyellHenrique <ranyellhenrique@gmail.com>
2020-03-16 00:16:29 -0300

    Projeto criado

---
+++ src/main/java/com/ranyell/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package com.ranyell.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
